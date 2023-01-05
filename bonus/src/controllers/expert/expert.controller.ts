a98b35b2856379702547167962e329ff8cab448d
bumpyLumps <alexander.fulop.art@gmail.com>
2023-01-04 22:43:28 -0500

    got rid of extra README in bonus dir

--- bonus/src/controllers/expert/expert.controller.ts
+++ bonus/src/controllers/expert/expert.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('expert')
-export class ExpertController {}
+export class ExpertController {
+
+    @Get()
+    @Render('')
+    root(){}
+}
